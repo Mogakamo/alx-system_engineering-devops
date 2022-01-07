01ce0069d944572c2210de214f3851dd9b8d31b4
Mogakamo <com02519@uoeld.ac.ke>
2022-01-07 05:22:38 -0800

    commit

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,7 +3,7 @@ include stdlib
 
 file_line { 'USE private key':
   path  =>  '/etc/ssh/ssh_config',
-  line  =>  'IdentityFile ~/.ssh/holberton',
+  line  =>  'IdentityFile ~/.ssh/school',
 }
 
 file_line { 'TURN OFF Password Auth':
